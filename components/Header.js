f167a11646765a8a070836afc3612cd8483c1572
T0modachi <jvergarava@gmail.com>
2024-09-29 12:25:22 +0200

    first version of the header

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,17 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
 
-export default function Header(){
+export default function Header() {
   return (
     <header>
-      <div className="container">
-        <Link href='/'>
-          <h2>T0modachi&apos;s Blog</h2>
-        </Link>
+      <div className="logo">
+        <Link href="/">T0modachi</Link>
+        <Image src="/logo_50_34.jpg" width={50} height={34} alt="Logo" />
+      </div>
+      <div className="nav-links">
+        <Link href="/posts">Posts</Link>
+        <Link href="/about">About</Link>
       </div>
     </header>
-  )
+  );
 }
